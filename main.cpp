ee58b38b8c9b9e4b6663801945834e516bbe492d
Malcon Rezende <rezendemalcon13@gmail.com>
2020-05-05 17:07:06 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main()
 {
     string sexo;
     int cerveja, refrigerante, espetinho;
+    double consumo, couvert, ingresso, Valor;
+
+    cout << fixed << setprecision(2);
 
     cout << "Sexo: ";
     cin >> sexo;
@@ -16,5 +19,32 @@ int main()
     cout << "Quantidade de espetinhos: ";
     cin >> espetinho;
 
+    consumo = (refrigerante * 3) + (espetinho * 7) + (cerveja * 5);
+
+    if (sexo == "F"){
+        ingresso = 8;
+    }
+    else {
+        ingresso = 10;
+    }
+    if (consumo >= 30){
+      couvert = 0;
+    }
+    else {
+        couvert = 4;
+    }
+    Valor = consumo + couvert + ingresso;
+
+    cout <<endl<< "RELATORIO: " <<endl;
+    cout << "Consumo = R$ " << consumo <<endl;
+    if (couvert == 0){
+        cout << "Isento de Couvert" <<endl;
+    }
+    else {
+        cout << "Couvert = R$ " << couvert <<endl;
+    }
+    cout << "Ingresso = R$ " << ingresso <<endl;
+    cout <<endl<< "Valor a pagar = R$ " << Valor <<endl;
     return 0;
 }
+
